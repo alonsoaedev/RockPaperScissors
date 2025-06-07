4d80bb5f02a228c9c4897d5dbd03232af24d82b4
Alonso Acosta <alonso.ae@hotmail.com>
2025-06-06 21:06:25 -0600

    first commit

---
+++ RockPaperScissors/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  RockPaperScissors
+//
+//  Created by Alonso Acosta Enriquez on 06/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Alonso Acosta Enriquez on 06/08/24.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
